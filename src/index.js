7bcd9815bb1a75a1fa0d8a361845a70e3ad4d536
King9969 <71065155+King9969@users.noreply.github.com>
2020-10-01 13:32:50 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <>
+    <App />
+    <h1>DM me will add your playlist to </h1>
+    <iframe
+      src="https://open.spotify.com/embed?uri=spotify%3Aplaylist%3A37i9dQZF1DX1ct2TQrAvRf"
+      width="400"
+      height="380"
+      frameborder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    ></iframe>
+  </>,
+  document.getElementById("root")
+);
