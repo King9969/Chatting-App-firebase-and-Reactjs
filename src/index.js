ff32243d931bdf534cad060aba94d7cc62821dbc
King9969 <71065155+King9969@users.noreply.github.com>
2020-10-12 22:20:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
 import "./index.css";
 
 ReactDOM.render(
-  <>
+  <div>
     <App />
-    <h1>DM me will add your playlist too  </h1>
-   <h1>login and refresh to play songs  </h1>
+    <iframe width="750" height="600" src="https://www.youtube.com/embed/pnkuI8KXW_8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <iframe
       src="https://open.spotify.com/embed?uri=spotify%3Aplaylist%3A37i9dQZF1DX1ct2TQrAvRf"
-      width="400"
-      height="380"
+      width="300"
+      height="600"
       frameborder="0"
       allowtransparency="true"
       allow="encrypted-media"
     ></iframe>
-  </>,
+
+  </div>,
   document.getElementById("root")
 );
